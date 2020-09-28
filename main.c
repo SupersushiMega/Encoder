6eeaa89839760df736075683a4dca1118ebc323f
SupersushiMega <sascha.rutz@outlook.com>
2020-09-28 18:26:37 +0200

    Added DAU proofing and added more Comments

--- main.c
+++ main.c
@@ -27,23 +27,39 @@ int main()
         if(MEDcoder != 1){
             MEDcoder = 0;
             printf("Wilkommen\n Dies ist ein Encoder/ Decoder.\n Waelen sie\n A fuer Encoder\n B fuer Decoder\n ");
-            scanf("%c", &MEDcoder);
-            getchar();
+
+            do
+            {
+                scanf("%c", &MEDcoder);
+                getchar();
+                if(!(MEDcoder == 'A' || MEDcoder == 'a' || MEDcoder == 'B' || MEDcoder == 'b')) //Check if the input is a allowed Letter
+                {
+                    printf("Eingabe muss entweder A oder B sein\n");    //Tell User that his input was a illegal letter
+                }
+            }while(!(MEDcoder == 'A' || MEDcoder == 'a' || MEDcoder == 'B' || MEDcoder == 'b'));    //Repeat if input is a illegal letter
         }
        switch(MEDcoder)
        {
         case 'A':
         case 'a'://modus encoder
             printf("Sie haben den Modus Encoder Gewaehlt.\n Bitte geben sie einen gewuenschten Schluessel zwischen 2 und 10 ein.\n Dieser wird zum Decoden des Textes benoetigt.\n ");
-            scanf("%d", &Key); //get variable key
-            getchar();
+            do
+            {
+                scanf("%d", &Key); //get variable key
+                getchar();
+                if(Key < 2 || Key > 10) //Check if the input is inside allowed Range
+                {
+                    printf("Schluessel auserhalb des erlaubten Bereichs Zahl muss zwischen 2 und 10 sein\n");   //Tell User that his input was outside the allowed area
+                }
+            }while(Key < 2 || Key > 10);    //Repeat if the input is outside allowed Range
+
             printf("Bitte geben sie die zu encodierende Nachricht ein\n");
             fgets(Input, 257, stdin);   //get message from user
             printf("%s", Input);
             encode(Key, Input); //Encode Message
 
-            printf("\nstrlen Output %d\nsize Output %d\n-------------------------------------------\n", strlen(Output), sizeof(Output));
-            printf("Ihre Encodierte Nachricht:\n%s\n", Output);
+            printf("\n-------------------------------------------\n");
+            printf("Ihre Encodierte Nachricht:\n%s\n", Output); //Output encoded Message
 
             getchar();
 
@@ -53,15 +69,24 @@ int main()
         case 'B':
         case 'b'://modus Decoder
             printf("Sie haben den Modus Decoder Gewaehlt.\n Bitte geben den zur Nachricht gehoerenden Schluessel ein.\n ");
-            scanf("%d", &Key); //get variable key
-            getchar();
+            do
+            {
+                scanf("%d", &Key); //get variable key
+                getchar();
+                if(Key < 2 || Key > 10) //Check if the input is inside allowed Range
+                {
+                    printf("Schluessel auserhalb des erlaubten Bereichs Zahl muss zwischen 2 und 10 sein\n");   //Tell User that his input was outside the allowed area
+                }
+            }while(Key < 2 || Key > 10);    //Repeat if the input is outside allowed Range
+
             printf("Bitte geben sie die zu decodierende Nachricht ein\n");
             fgets(Input, 270, stdin);   //get Encoded message from user
             printf("%s", Input);
-            decode(Key, Input);
-            printf("Ihre Decodierte Nachricht:\n%s\n", Output);
+            decode(Key, Input); //Decode Message
 
-            //decode(Key, Input);
+            printf("\n-------------------------------------------\n");
+            printf("Ihre Decodierte Nachricht:\n%s\n", Output); //Output decoded Message
+
             getchar();
 
             MEDcoder = 1;
@@ -69,8 +94,16 @@ int main()
 
         case 1:
             printf("Programm beenden = y\n fortfahren = enter\n ");
-            scanf("%c", &MEDcoder);
-            getchar();
+            do
+            {
+                MEDcoder = 1;
+                scanf("%c", &MEDcoder);
+                getchar();
+                if(!(MEDcoder == 'y' || MEDcoder == '\n'))  //Check if the input is a allowed
+                {
+                    printf("Eingabe muss entweder y oder nichts sein\n");   //Tell User that his input was a illegal letter
+                }
+            }while(!(MEDcoder == 'y' || MEDcoder == '\n')); //Repeat if input is a illegal letter
             break;
        }//end switch case
     }//end while
@@ -99,22 +132,13 @@ void encode(short key, char Input[])   //encoding key is row count, Input is inp
             {
                 stringPos++;
             }
-            if((stringPos <= LengthInput))
+            if(stringPos <= LengthInput)    //Check if the entire string has been written
             {
-                Output2D[Row][RowPos] = Input[stringPos];
-                printf("Output2D[%d][%d] %c    Input[%d] %c\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Input[stringPos]);
+                Output2D[Row][RowPos] = Input[stringPos];   //Write Letter to Output string
                 stringPos++;
             }
         }
-        printf("\n");
     }
-    printf("LengthInput %d\n", LengthInput);
-    printf("Stringpos %d\n", stringPos);
-    printf("RowPos %d\n", RowPos);
-    printf("RowLength %d\n", RowLength);
-    printf("Row %d\n", Row);
-    printf("Key %d\n", key);
-    printf("\n");
     //===============================================================================================
 
     //Write 2D array to global string Output
@@ -126,37 +150,33 @@ void encode(short key, char Input[])   //encoding key is row count, Input is inp
         {
             while((Output2D[Row][RowPos] >= 0) && (Output2D[Row][RowPos] <= 31) && (Row < key))  //Filter out non printable characters
             {
-                printf("(Filter)Output2D[%d][%d] %c    Output[%d] %d\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Output[stringPos]);
                 RowPos++;
-                if(RowPos > RowLength)
+                if(RowPos > RowLength)  //Check if row is finished
                 {
-                    printf("\n");
-                    RowPos = 0;
-                    Row++;
+                    RowPos = 0; //set row position start
+                    Row++;  //go to next row
                 }
             }
-            if (Row < key)
+            if (Row < key)  //Check if Row is not to high
             {
-                Output[stringPos] = Output2D[Row][RowPos];
-                printf("Output2D[%d][%d] %c    Output[%d] %d\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Output[stringPos]);
+                Output[stringPos] = Output2D[Row][RowPos];  //Write Character to Output String
                 stringPos++;
             }
 
-            if((stringPos < LengthInput))
+            if(stringPos < LengthInput) //Check if the entire string has been written
             {
-                Output[stringPos] = Output2D[Row][RowPos];
+                Output[stringPos] = Output2D[Row][RowPos];  //Write Letter to Output string
             }
             else
             {
-                Output[stringPos] = 0;
+                Output[stringPos] = 0;  //Write null to Output string to define end of string
             }
         }
-        printf("\n");
     }
     //===============================================================================================
 }
 
-void decode(short key, char Input[])   //encoding key is row count, Input is input to encode
+void decode(short key, char Input[])   //decoding key is row count, Input is input to encode
 {
     short LengthInput = strlen(Input);  //Length of input string
     short RowLength = ceil((float)LengthInput / (float)key);   //Length of Row
@@ -176,29 +196,15 @@ void decode(short key, char Input[])   //encoding key is row count, Input is inp
         {
             while((Input[stringPos] >= 0) && (Input[stringPos] <= 31) && (stringPos <= LengthInput))  //Filter out non printable characters
             {
-                printf("(Filter)Output2D[%d][%d] %c    Input[%d] %c\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Input[stringPos]);
                 stringPos++;
             }
-            if((stringPos <= LengthInput))
+            if(stringPos <= LengthInput)    //Check if the entire string has been written
             {
                 Output2D[Row][RowPos] = Input[stringPos];
-                printf("Output2D[%d][%d] %c    Input[%d] %c\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Input[stringPos]);
                 stringPos++;
             }
-            else
-            {
-                printf("(FilterIf)Output2D[%d][%d] %c    Input[%d] %c\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Input[stringPos]);
-            }
         }
-        printf("\n");
     }
-    printf("LengthInput %d\n", LengthInput);
-    printf("Stringpos %d\n", stringPos);
-    printf("RowPos %d\n", RowPos);
-    printf("RowLength %d\n", RowLength);
-    printf("Row %d\n", Row);
-    printf("Key %d\n", key);
-    printf("\n");
     //===============================================================================================
 
     //Write 2D array to global string Output
@@ -210,24 +216,19 @@ void decode(short key, char Input[])   //encoding key is row count, Input is inp
         {
             while((Output2D[Row][RowPos] >= 0) && (Output2D[Row][RowPos] <= 31))  //Filter out non printable characters
             {
-                printf("(Filter)Output2D[%d][%d] %c    Output[%d] %d\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Output[stringPos]);
                 Row++;
-                if(Row == key)
+                if(Row == key)  //Check if last row has been reached
                 {
-                    printf("\n");
-                    Row = 0;
-                    RowPos++;
+                    Row = 0;    //Go to Roq 0
+                    RowPos++;   //Go to next row position
                 }
             }
-            if((stringPos < LengthInput))
+            if(stringPos < LengthInput) //Check if the entire string has been written
             {
-                Output[stringPos] = Output2D[Row][RowPos];
+                Output[stringPos] = Output2D[Row][RowPos];  //Write Letter to Output string
             }
-
-            printf("Output2D[%d][%d] %c    Output[%d] %d\n", Row, RowPos, Output2D[Row][RowPos], stringPos, Output[stringPos]);
             stringPos++;
         }
-        printf("\n");
     }
     //===============================================================================================
 }
